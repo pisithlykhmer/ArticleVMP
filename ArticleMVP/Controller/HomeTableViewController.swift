b3fe122d1245f6f3f5cf7b09bb4c3356b655e725
Pisith Ly Khmer <kshrd@kshrds-imac-2.local>
2016-12-17 11:01:06 +0700

    Second Commit

---
+++ ArticleMVP/Controller/HomeTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeTableViewController.swift
+//  ArticleMVP
+//
+//  Created by KSHRD on 12/17/16.
+//  Copyright © 2016 KSHRD. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
